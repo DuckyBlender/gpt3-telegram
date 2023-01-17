3855c23cd999e85ceb04074af013ca3e51198b8a
DuckyBlender <alan@alanklas.com>
2023-01-17 16:35:53 +0100

    added save command and polished a ton of stuff, changed model

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import sqlite3
 from telebot import apihelper
 # import the thing that adds buttons
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+# split the text into chunks
+from telebot import util
 
 load_dotenv()
 
@@ -17,8 +19,16 @@ openai.api_key = os.environ.get("OPENAI_KEY")
 bot = telebot.TeleBot(TOKEN)
 
 LIMIT = 100
+TIMEOUT = 5  # Minutes
+
+MAX_TOKENS = 150
+TEMPERATURE = 0.7
+#MODEL = "text-davinci-003"
+MODEL = "text-curie-001"
 
 # /reset - Reset the chatbot
+
+
 @bot.message_handler(commands=['reset'])
 def reset(message):
     # Save the message count to a variable
@@ -30,20 +40,24 @@ def reset(message):
     cursor = con.cursor()
 
     # Get the message count
-    cursor.execute("SELECT message_count FROM users WHERE user_id = ?", (user_id,))
+    cursor.execute(
+        "SELECT message_count FROM users WHERE user_id = ?", (user_id,))
     message_count = cursor.fetchone()[0]
 
     # Delete the user
     cursor.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
 
     # Insert the user again with the message count
-    cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (user_id, "", message_count, now))
+    cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)",
+                   (user_id, "", message_count, now))
 
     con.commit()
     con.close()
-    
+
     # Send the user a message to start the chat
-    bot.reply_to(message, 'Hi, I am a chatbot. How can I help you?', parse_mode='Markdown')
+    bot.reply_to(message, 'Hi, I am a chatbot. How can I help you?',
+                 parse_mode='Markdown')
+
 
 @bot.message_handler(commands=['limit'])
 def limit(message):
@@ -51,12 +65,36 @@ def limit(message):
     con = sqlite3.connect('users.db')
     cursor = con.cursor()
     # Get the message count
-    cursor.execute("SELECT message_count FROM users WHERE user_id = ?", (user_id,))
+    cursor.execute(
+        "SELECT message_count FROM users WHERE user_id = ?", (user_id,))
     message_count = cursor.fetchone()[0]
     con.close()
-    bot.reply_to(message, f"You have used `{message_count}`/`{LIMIT}` messages.", parse_mode='Markdown')
+    bot.reply_to(
+        message, f"You have used `{message_count}`/`{LIMIT}` messages.", parse_mode='Markdown')
 
-# Get message from user and send it to the OpenAI API to get a response back. Keep the conversation going until the user does not respond for 5 minutes.
+# Save command - saves the conversation to a txt file and sends it to the user
+@bot.message_handler(commands=['save'])
+def save(message):
+    # Get the user id
+    user_id = message.from_user.id
+    # Connect to the database and get the chat messages
+    con = sqlite3.connect('users.db')
+    cursor = con.cursor()
+    cursor.execute("SELECT chat_messages FROM users WHERE user_id = ?", (user_id,))
+    chat_messages = cursor.fetchone()[0]
+    # Remove whitespaces from the beginning and end of the string
+    chat_messages = chat_messages.strip()
+    con.close()
+    # Save the chat messages to a txt file
+    with open(f"{user_id}.txt", "w") as f:
+        f.write(chat_messages)
+    # Send the txt file to the user
+    bot.reply_to(message, "Here is our conversation so far. Would you like to clear the conversation and start over? If so, use the /reset command.")
+    bot.send_document(message.chat.id, open(f"{user_id}.txt", 'rb'))
+    # Delete the txt file
+    os.remove(f"{user_id}.txt")    
+
+# Get message from user and send it to the OpenAI API to get a response back. Keep the conversation going until the user does not respond for TIMEOUT minutes.
 @bot.message_handler(func=lambda msg: True)
 def chat(message):
     # Log the users chat id to the database
@@ -72,21 +110,24 @@ def chat(message):
         message_count = user[2]
         # Check if the user has reached the message limit
         if message_count >= LIMIT:
-            bot.reply_to(message, f"You have reached the message limit of {LIMIT} messages.")
+            bot.reply_to(
+                message, f"You have reached the message limit of {LIMIT} messages.")
             return
         # Get the chat messages
         chat_messages = user[1]
         # Append the new message
         chat_messages += f"\nUser: {message.text}"
         # Update the chat messages
-        cursor.execute("UPDATE users SET chat_messages = ? WHERE user_id = ?", (chat_messages, user_id))
+        cursor.execute(
+            "UPDATE users SET chat_messages = ? WHERE user_id = ?", (chat_messages, user_id))
     else:
         # Insert the user
         chat_messages = f"\nUser: {message.text}"
         # Current date and time
         now = datetime.datetime.now()
-        cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (user_id, chat_messages, 0, now))
-    
+        cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)",
+                       (user_id, chat_messages, 0, now))
+
     con.commit()
     con.close()
 
@@ -99,39 +140,42 @@ def chat(message):
 
     # Get response from OpenAI API
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model=MODEL,
         prompt=chat_messages,
         stop=["\nUser:", "\nBot:"],
-        max_tokens=150,
-        temperature=0.7
+        max_tokens=MAX_TOKENS,
+        temperature=TEMPERATURE,
     )
 
     # Append the response to the chat messages. Remove the one whitespace at the beginning of the response.
     response = response['choices'][0]['text']
+    # Remvove whitespaces from the beginning and end of the response, but add one at the beginning
+    response = response.strip()
+    response = f" {response}"
+    
     chat_messages += response
 
     # Update the chat messages in the database and the message count
     con = sqlite3.connect('users.db')
     cursor = con.cursor()
-    cursor.execute("UPDATE users SET chat_messages = ? WHERE user_id = ?", (chat_messages, user_id))
-    cursor.execute("UPDATE users SET message_count = message_count + 1 WHERE user_id = ?", (user_id,))
+    cursor.execute(
+        "UPDATE users SET chat_messages = ? WHERE user_id = ?", (chat_messages, user_id))
+    cursor.execute(
+        "UPDATE users SET message_count = message_count + 1 WHERE user_id = ?", (user_id,))
     con.commit()
     con.close()
 
     print(f"===\nUSER ID: {user_id}\n{chat_messages}\n===")
-    # Format the response (for debugging purposes). Remove the one whitespace at the beginning of the response. 
-    final_response = f"```{convo}```\n`Bot:` *{response[1:]}*"
-    # If the final response is longer than 4096 characters, split it into multiple messages
-    if len(final_response) > 4096:
-        # Split the response into multiple messages
-        messages = [final_response[i:i+4096] for i in range(0, len(final_response), 4096)]
-        # Send the messages
-        for message in messages:
-            bot.send_message(message.chat.id, message, parse_mode='Markdown')
-    else:
-        bot.send_message(message.chat.id, final_response, parse_mode='Markdown')
-
-# TODO: Buttons to reset the chatbot and to see the message limit
+    # Format the response (for debugging purposes). Remove all whitespaces (spaces and newlines) from the beginning and end of the response.
+    response = response.strip()
+    # final_response = f"```{convo}```\n`Bot:` *{response}*"
+    
+    # Shorten the convo variable if it is longer then 4000 characters.
+    if len(convo) > 3800:
+        convo = f"(message shortened because of message length limit. to show entire conversation execute /save)\n{convo[-3800:]}"
+    # Send the response to the user
+    bot.send_message(message.chat.id, f"```{convo}```\n`Bot:` *{response}*", parse_mode='Markdown')
+        
 
 
 # For debugging purpouses, remove the user.db at start
@@ -143,16 +187,19 @@ if not os.path.exists('users.db'):
     print("Creating users.db file...")
     con = sqlite3.connect('users.db')
     cursor = con.cursor()
-    cursor.execute("CREATE TABLE users (user_id INTEGER, chat_messages TEXT, message_count INTEGER, date DATETIME)")
+    cursor.execute(
+        "CREATE TABLE users (user_id INTEGER, chat_messages TEXT, message_count INTEGER, date DATETIME)")
     # Log table
-    cursor.execute("CREATE TABLE log (user_id INTEGER, message TEXT, date DATETIME)")
+    cursor.execute(
+        "CREATE TABLE log (user_id INTEGER, message TEXT, date DATETIME)")
     # Create a trigger to log the messages to the log table if a user is removed from the users table
-    cursor.execute("CREATE TRIGGER log_delete AFTER DELETE ON users BEGIN INSERT INTO log VALUES (old.user_id, old.chat_messages, datetime('now')); END;")
+    cursor.execute(
+        "CREATE TRIGGER log_delete AFTER DELETE ON users BEGIN INSERT INTO log VALUES (old.user_id, old.chat_messages, datetime('now')); END;")
     con.commit()
     con.close()
 
 # Update the slash commands on the server using requests
-requests.post("https://api.telegram.org/bot{}/setMyCommands".format(TOKEN), json={
+requests.post(f"https://api.telegram.org/bot{TOKEN}/setMyCommands", json={
     "commands": [
         {
             "command": "reset",
@@ -161,6 +208,10 @@ requests.post("https://api.telegram.org/bot{}/setMyCommands".format(TOKEN), json
         {
             "command": "limit",
             "description": "View your message limit"
+        },
+        {
+            "command": "save",
+            "description": "Save the chat to a txt file"
         }
     ]
 })
