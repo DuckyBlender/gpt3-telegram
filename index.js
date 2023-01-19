86ee6beb04cd828ccfde611880c55175f2fa71cd
DuckyBlender <alan@alanklas.com>
2023-01-19 01:42:50 +0100

    still doesnt work, info and help works tho

---
+++ index.js
@@ -0,0 +1,231 @@
+const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+const { Configuration, OpenAIApi } = require("openai");
+const sqlite3 = require("sqlite3").verbose();
+
+dotenv.config();
+
+const LIMIT = 100;
+const TIMEOUT = 5;
+
+const MAX_TOKENS = 150;
+const TEMPERATURE = 0.7;
+const MODEL = "text-davinci-003";
+// const MODEL = "text-curie-001";
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(config);
+
+const bot = new Telegraf(process.env.TELEGRAM_KEY);
+
+const db = new sqlite3.Database("./users.db");
+// Insert these tables and triggers
+// "CREATE TABLE IF NOT EXISTS users (user_id INTEGER, chat_messages TEXT, message_count INTEGER, date DATETIME)"
+// "CREATE TABLE log (user_id INTEGER, message TEXT, date DATETIME)"
+// "CREATE TRIGGER log_delete AFTER DELETE ON users BEGIN INSERT INTO log VALUES (old.user_id, old.chat_messages, datetime('now')); END;")
+
+db.run(
+    "CREATE TABLE IF NOT EXISTS users (user_id INTEGER, chat_messages TEXT, message_count INTEGER, date DATETIME)"
+);
+db.run(
+    "CREATE TABLE IF NOT EXISTS log (user_id INTEGER, message TEXT, date DATETIME)"
+);
+db.run(
+    "CREATE TRIGGER IF NOT EXISTS log_delete AFTER DELETE ON users BEGIN INSERT INTO log VALUES (old.user_id, old.chat_messages, datetime('now')); END;"
+);
+
+// Update the slash commands
+bot.telegram.setMyCommands([
+    { command: "/help", description: "Show this message" },
+    { command: "/info", description: "Show info about the bot" },
+    { command: "/ask", description: "Ask the bot a question" },
+    { command: "/reset", description: "Reset the chatbot" },
+    {
+        command: "/limit",
+        description:
+            "Show how many messages you have left (message limit resets every midnight UTC)",
+    },
+    { command: "/save", description: "Save the conversation to a txt file" },
+]);
+
+// Help command
+bot.command("help", (ctx) => {
+    const help_text = `Hi, I'm a chatbot (\`${MODEL}\` model from OpenAI). To get started, just type something in the chat!\n*You can use the following commands:*\n/start - Start the chatbot\n/help - Show this message\n/limit - Show how many messages you have left (message limit resets every midnight UTC)\n/save - Save the conversation to a txt file\n/reset - Reset the chatbot`;
+    ctx.replyWithMarkdown(help_text);
+});
+
+bot.command("info", (ctx) => {
+    const info_text = `Model: \`${MODEL}\`\nMax tokens: \`${MAX_TOKENS}\`\nTemperature: \`${TEMPERATURE}\`\nMessage limit: \`${LIMIT}\`\nMessage timeout: \`${TIMEOUT}\``;
+    ctx.replyWithMarkdown(info_text);
+});
+
+bot.command("reset", (ctx) => {
+    // Get users ID
+    const user_id = ctx.message.from.id;
+    // Get current time
+    const date = new Date().toISOString().slice(0, 19).replace("T", " ");
+    // If the user exists in the database, fetch his message count and store it in a variable. Delete the user from the database and insert a new user with the same ID and the same message count.
+    db.get(`SELECT * FROM users WHERE user_id = ${user_id}`, (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (row) {
+                const message_count = row.message_count;
+                db.run(`DELETE FROM users WHERE user_id = ${user_id}`);
+                db.run(
+                    `INSERT INTO users VALUES (${user_id}, '', ${message_count}, '${date}')`
+                );
+                ctx.reply(
+                    "Chat history reset! Your message count has not been reset."
+                );
+            } else {
+                ctx.reply("You have not started a conversation yet!");
+            }
+        }
+    });
+});
+
+bot.command("limit", (ctx) => {
+    // Get users ID
+    const user_id = ctx.message.from.id;
+    // Get the users message count from the database
+    db.get(`SELECT * FROM users WHERE user_id = ${user_id}`, (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (row) {
+                const message_count = row.message_count;
+                if (message_count < LIMIT) {
+                    ctx.reply(
+                        `You have ${LIMIT - message_count} messages left.`
+                    );
+                } else {
+                    ctx.reply(
+                        `You have reached the message limit of ${LIMIT} messages. Please wait until midnight UTC to send more messages.`
+                    );
+                }
+            } else {
+                ctx.reply("You have not started a conversation yet!");
+            }
+        }
+    });
+});
+
+bot.command("save", (ctx) => {
+    // Get users ID
+    const user_id = ctx.message.from.id;
+    // Get the users messages from the database
+    db.get(`SELECT * FROM users WHERE user_id = ${user_id}`, (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (row) {
+                const chat_messages = row.chat_messages;
+                // Create a file with the users messages
+                fs.writeFile(`./${user_id}.txt`, chat_messages, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        // Send the file to the user with a message
+                        ctx.replyWithDocument(
+                            { source: `./${user_id}.txt` },
+                            { caption: "Here is your chat history:" }
+                        );
+                        // Delete the file after it has been sent
+                        fs.unlink(`./${user_id}.txt`, (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    }
+                });
+            } else {
+                ctx.reply("You have not started a conversation yet!");
+            }
+        }
+    });
+});
+
+bot.command("ask", (ctx) => {
+    // Get users ID
+    const user_id = ctx.message.from.id;
+    // Get the users message count from the database
+    // Check if the user exists in the database
+
+    db.get(`SELECT * FROM users WHERE user_id = ${user_id}`, (err, row) => {
+        if (err) {
+            console.log(err);
+            // Return an error message if there is an error
+            ctx.replyWithMarkdown(
+                `An error occured. Please try again later.\nError: \`${err}\``
+            );
+        } else {
+            let message_count = 0;
+            let chat_messages = "";
+            if (row) {
+                message_count = row.message_count;
+                chat_messages = row.chat_messages;
+            } else {
+                // Create a new user in the database
+                const date = new Date()
+                    .toISOString()
+                    .slice(0, 19)
+                    .replace("T", " ");
+                message_count = 0;
+                chat_messages = "";
+                db.run(
+                    `INSERT INTO users VALUES (${user_id}, "", ${message_count}, '${date}')`
+                );
+            }
+            // If the user has not reached the message limit, send the message to OpenAI and send the response back to the user
+            if (message_count < LIMIT) {
+                // Get the users message
+                const message = ctx.message.text.split(" ").slice(1).join(" ");
+                // Format the request to OpenAI
+                const request = `You are a super advanced AI. You are talking to a human through a chat interface. Try to have a conversation with the human, while pretending to be a human yourself.\nHuman: ${message}\nAI:`;
+                // Send the message to OpenAI
+                const response = openai.createCompletion({
+                    engine: MODEL,
+                    prompt: request,
+                    temperature: TEMPERATURE,
+                    maxTokens: MAX_TOKENS,
+					stop: ["\nHuman:", "\nAI:"],
+                });
+                // Send the response back to the user
+                response.then((data) => {
+					const reply = data.data.choices[0].text;
+					ctx.reply(reply);
+					// Update the users message count in the database
+					db.run(
+						`UPDATE users SET message_count = message_count + 1 WHERE user_id = ${user_id}`
+					);
+				});
+            } else {
+                ctx.reply(
+                    `You have reached the message limit of ${LIMIT} messages. Please wait until midnight UTC to send more messages.`
+                );
+            }
+        }
+    });
+});
+
+// bot.start((ctx) => ctx.reply('Welcome'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on('sticker', (ctx) => ctx.reply('👍'));
+// bot.command('test', (ctx) => {
+//     // get the command arguments
+//     const args = ctx.message.text.split(' ').slice(1);
+//     // send the command arguments back
+//     ctx.reply(args.join(' '));
+//     ctx.replyWithMarkdownV2('Hello *World*');
+// })
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+// Launch the bot
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
